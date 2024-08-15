38392aeb20273194a6ea739131054cc0b39f37d7
leemyou <limhyeon0809@gmail.com>
2024-08-15 19:53:45 +0900

    FEAT : todo 추가 기능
    - todo 리스트의 요소 추가하는 기능 추가

--- src/hooks/fetch.js
+++ src/hooks/fetch.js
@@ -8,7 +8,8 @@ export const getFetchList = async () => {
       throw new Error(`HTTP GET error! status: ${res.status}`);
     }
 
-    const data = res.json();
+    const data = await res.json();
+    sessionStorage.setItem("todos", JSON.stringify(data)); // JSON 문자열로 저장해야한다!!
     return data;
   } catch (error) {
     console.error("Error:", error);
@@ -19,14 +20,18 @@ export const getFetchList = async () => {
 export const postFetchList = async (todoContents) => {
   console.log(todoContents);
   try {
+    const now = new Date();
     const res = await fetch(basicUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        userId: 1,
         title: todoContents,
         completed: false,
+        createdAt: now,
+        updatedAt: now,
       }),
     });
 
@@ -34,8 +39,16 @@ export const postFetchList = async (todoContents) => {
       throw new Error(`HTTP POST error! status: ${res.status}`);
     }
 
-    const originData = console.log(await res.json());
-    console.log(await getFetchList());
+    const originData = JSON.parse(sessionStorage.getItem("todos")) || []; // 문자열로 저장된 데이터를 다시 JSON으로 파싱
+    const newData = {
+      ...(await res.json()),
+      id: originData[originData.length - 1].id + 1,
+    };
+    const updatedData = [...originData, newData];
+    sessionStorage.setItem("todos", JSON.stringify(updatedData)); // 업데이트된 데이터를 세션에 저장
+    console.log(updatedData);
+    // return updatedData;
+    return newData;
   } catch (error) {
     console.error("Error:", error);
     return [];
--- src/pages/list.js
+++ src/pages/list.js
@@ -3,7 +3,6 @@ import checkIcon from "../assets/icons/check.png";
 import { postFetchList } from "../hooks/fetch";
 
 export const getList = (list) => {
-  console.log(list);
   const listWrapper = document.querySelector("#todoList");
 
   return list.map((item) => {
@@ -48,8 +47,45 @@ export const getList = (list) => {
 export const postList = (element) => {
   const contents = document.getElementById("inputNewTodo");
 
-  element.addEventListener(
-    "click",
-    async () => await postFetchList(contents.value)
-  );
+  element.addEventListener("click", async () => {
+    try {
+      const newData = await postFetchList(contents.value);
+      const listWrapper = document.querySelector("#todoList");
+
+      const newList = document.createElement("li");
+      newList.innerHTML = `
+          <label for="todo-${newData.id}">
+            <input type="checkbox" id="todo-${newData.id}" />
+            
+            <div class="checkbox button-shadow ${
+              newData.completed ? "checkbox-checked" : "checkbox-non-checked"
+            }">
+              <div class="checked-wrapper inner-shadow">
+                <img src=${checkIcon} />
+              </div>
+            </div>
+            <p>${newData.title}</p>
+          </label>
+          
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="ionicon"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="32"
+                d="M368 368L144 144M368 144L144 368"
+              />
+            </svg>
+          </button>
+      `;
+
+      listWrapper.appendChild(newList);
+    } catch (error) {}
+  });
 };
