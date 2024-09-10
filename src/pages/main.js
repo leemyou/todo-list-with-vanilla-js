45b6d824a632130da8453561580c99ecafc54282
leemyou <limhyeon0809@gmail.com>
2024-09-10 18:45:34 +0900

    REFECTOR : enter코드 제거
    - 기능 동작 안하는 enter 코드 제거

--- src/pages/main.js
+++ src/pages/main.js
@@ -10,15 +10,4 @@ getList(await getFetchList());
 // 리스트 등록 함수
 postList(document.getElementById("addList"));
 
-const pressEnter = () => {
-  // 엔터키의 코드는 13입니다.
-  if (event.keyCode == 13) {
-    // 실행할 함수();
-    console.log("run");
-    return;
-  }
-};
-
 modalOpen(document.getElementById("modeInfo"));
-
-export { pressEnter };
