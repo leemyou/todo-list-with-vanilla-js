a42c6bf7d61346610a123022702d43d90e2c9540
leemyou <limhyeon0809@gmail.com>
2024-09-11 18:46:09 +0900

    TEST : axios 테스트 코드 추가

---
+++ src/hooks/axios.js
@@ -0,0 +1,29 @@
+const basicUrl = "http://localhost:3000/todos";
+
+const getAxiosList = async () => {
+  console.log("run axios Mode");
+};
+
+const serchAxiosList = async () => {
+  console.log("run axios Mode");
+};
+
+const postAxiosList = async () => {
+  console.log("run axios Mode");
+};
+
+const putAxiosList = async () => {
+  console.log("run axios Mode");
+};
+
+const deleteAxiosList = async () => {
+  console.log("run axios Mode");
+};
+
+export {
+  getAxiosList,
+  serchAxiosList,
+  postAxiosList,
+  putAxiosList,
+  deleteAxiosList,
+};
