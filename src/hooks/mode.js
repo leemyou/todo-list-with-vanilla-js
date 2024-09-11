2cc8073bd7c1e3b7a70c047b8d761c16aac2b1eb
leemyou <limhyeon0809@gmail.com>
2024-09-11 17:55:29 +0900

    REFECTOR : 모드 변경 함수 추가
    - 모드 변경 함수 추가
    - currentMode의 DB구조 변경(boolean으로 간편하게 처리하기 위해 currentMode => isFetchMode 변경)

--- src/hooks/mode.js
+++ src/hooks/mode.js
@@ -1,26 +1,77 @@
 import axios from "axios";
 
-const basicUrl = "http://localhost:3000";
+const basicUrl = "http://localhost:3000/currentMode";
 
+/**
+ * 최근 모드의 전체 정보 불러오는 함수
+ * @returns currentMode
+ */
 const getCurrentMode = async () => {
   try {
-    const { data } = await axios.get(basicUrl + "/currentMode");
+    const { data } = await axios.get(basicUrl);
     return data;
   } catch (error) {
     console.log("GET CURRENTMODE ERROR! :", error);
   }
 };
 
+/**
+ * 최근 모드의 데이터를 불러오는 함수
+ * @returns modeInfo
+ */
 const getCurrentModeData = async () => {
   try {
     const currentMode = await getCurrentMode();
-    const { data } = await axios.get(basicUrl + "/modeInfo");
-    return data.find((obj) => obj.mode === currentMode);
+
+    return currentMode.modeInfo.find((obj) =>
+      currentMode.isFetchMode ? obj.mode === "FETCH" : obj.mode === "AXIOS"
+    );
   } catch (error) {
     console.log("GET getCurrentModeData ERROR! :", error);
   }
 };
 
-const changeCurrentMode = async () => {};
+/**
+ * 초기 모드를 불러오는 함수
+ */
+const getInitialMode = async (element) => {
+  const modeText = document.getElementById("modeInfo");
+  try {
+    const { mode } = await getCurrentModeData();
+    modeText.innerText = `${mode} MODE`;
+    return;
+  } catch (error) {
+    console.log("GET INITIALMODE ERROR! :", error);
+  }
+};
 
-export { getCurrentModeData, changeCurrentMode };
+/**
+ * 모드 변경함수
+ * @param {HTMLElement} element
+ */
+const changeCurrentMode = (element) => {
+  const modeText = document.getElementById("modeInfo");
+
+  element.addEventListener("click", async () => {
+    try {
+      const { isFetchMode, modeInfo } = await getCurrentMode();
+
+      const putData = {
+        modeInfo: modeInfo,
+        isFetchMode: !isFetchMode,
+      };
+
+      const { data } = await axios.put(basicUrl, putData);
+
+      console.log("currentMode: ", data);
+
+      const { mode } = await getCurrentModeData();
+      modeText.innerText = `${mode} MODE`;
+      return;
+    } catch (error) {
+      console.error("CHANGE CURRENTMODE ERROR! :", error);
+    }
+  });
+};
+
+export { getCurrentModeData, getInitialMode, changeCurrentMode };
--- src/pages/main.js
+++ src/pages/main.js
@@ -2,12 +2,19 @@ import "./mainStyle.css";
 
 import { getList, postList } from "../hooks/list";
 import { getFetchList } from "../hooks/fetch";
-import { modalClose, modalOpen } from "../hooks/modal";
+import { modalOpen } from "../hooks/modal";
+import { changeCurrentMode, getInitialMode } from "../hooks/mode";
 
 // 초기 리스트 불러오는 함수
 getList(await getFetchList());
 
 // 리스트 등록 함수
 postList(document.getElementById("addList"));
+
+// 초기 모드 불러오는 함수
+getInitialMode();
+
+// 모드 변경 함수
+changeCurrentMode(document.getElementById("btnModeChange"));
 
 modalOpen(document.getElementById("modeInfo"));
