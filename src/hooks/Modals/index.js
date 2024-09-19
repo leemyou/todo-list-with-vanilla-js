b17b61060d3ac1035bf11d4cfad2adc2625aa612
leemyou <limhyeon0809@gmail.com>
2024-09-19 17:08:58 +0900

    RENAME : 모달 파일 구조 변경
    - index 파일에 있는 내용 수정
    - modalModule 파일명 수정

--- src/hooks/Modals/index.js
+++ src/hooks/Modals/index.js
@@ -1,2 +1,12 @@
+import { createModal } from "../../pages/Modal/modalUi";
 export * from "./modal";
-export * from "./modalModule";
+
+// DOMContentLoaded 이벤트를 사용해 DOM 요소가 완전히 로드된 후 이벤트 리스너를 등록
+document.addEventListener("DOMContentLoaded", () => {
+  const modeInfoElement = document.getElementById("modeInfo");
+
+  // modeInfoElement가 존재할 경우에만 이벤트 리스너 등록
+  if (modeInfoElement) {
+    modeInfoElement.addEventListener("click", createModal);
+  }
+});
